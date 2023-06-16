dc6ba1342cd68da04c15dce42b1082b59679e403
tanjumtanha <tanha.tanni27@gmail.com>
2023-06-16 20:31:32 +0600

    jwt token done

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 //import axios from "axios";
 
@@ -50,17 +51,17 @@ const AuthProvider = ({ children }) => {
             setLoading(false);
 
             // get and set token
-            // if (currentUser) {
-            //     axios.post('https://bistro-boss-server-fawn.vercel.app/jwt', { email: currentUser.email })
-            //         .then(data => {
-            //             // console.log(data.data.token)
-            //             localStorage.setItem('access-token', data.data.token)
-            //             setLoading(false);
-            //         })
-            // }
-            // else {
-            //     localStorage.removeItem('access-token')
-            // }
+            if (currentUser) {
+                axios.post('http://localhost:5000/jwt', { email: currentUser.email })
+                    .then(data => {
+                        console.log(data.data.token)
+                        localStorage.setItem('access-token', data.data.token)
+                        setLoading(false);
+                    })
+            }
+            else {
+                localStorage.removeItem('access-token')
+            }
 
 
         });
