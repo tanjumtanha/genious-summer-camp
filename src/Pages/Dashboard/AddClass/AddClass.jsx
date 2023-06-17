a6a7ddb34e99f3f518e7dfa7877b8bded64bbefc
tanjumtanha <tanha.tanni27@gmail.com>
2023-06-17 11:10:52 +0600

    image hosting work done

--- src/Pages/Dashboard/AddClass/AddClass.jsx
+++ src/Pages/Dashboard/AddClass/AddClass.jsx
@@ -1,57 +1,58 @@
-//import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthContext } from '../../../providers/AuthProvider';
-//import useAxiosSecure from "../../../hooks/useAxiosSecure";
-//import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import { Helmet } from 'react-helmet-async';
+import Title from '../../../components/Title/Title';
 
-//const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
+const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
 
 const AddClass = () => {
-    const {user}=useContext(AuthContext);
-    //const [axiosSecure] = useAxiosSecure();
+    const { user } = useContext(AuthContext);
+    const [axiosSecure] = useAxiosSecure();
     const { register, handleSubmit, reset } = useForm();
-    //const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
+    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
 
     const onSubmit = data => {
         console.log(data)
-        // const formData = new FormData();
-        // formData.append('image', data.image[0])
+        const formData = new FormData();
+        formData.append('image', data.image[0])
 
-        // fetch(img_hosting_url, {
-        //     method: 'POST',
-        //     body: formData
-        // })
-        // .then(res => res.json())
-        // .then(imgResponse => {
-        //     if(imgResponse.success){
-        //         const imgURL = imgResponse.data.display_url;
-        //         const {name, price, category, recipe} = data;
-        //         const newItem = {name, price: parseFloat(price), category, recipe, image:imgURL}
-        //         console.log(newItem)
-        //         axiosSecure.post('/menu', newItem)
-        //         .then(data => {
-        //             console.log('after posting new menu item', data.data)
-        //             if(data.data.insertedId){
-        //                 reset();
-        //                 Swal.fire({
-        //                     position: 'top-end',
-        //                     icon: 'success',
-        //                     title: 'Item added successfully',
-        //                     showConfirmButton: false,
-        //                     timer: 1500
-        //                   })
-        //             }
-        //         })
-        //     }
-        // })
+        fetch(img_hosting_url, {
+            method: 'POST',
+            body: formData,
+        })
+            .then(res => res.json())
+            .then(imgResponse => {
+                console.log(imgResponse)
+                if (imgResponse.success) {
+                    const imgURL = imgResponse.data.display_url;
+                    const { name, price, availableSeats, instructor } = data;
+                    const newItem = { name, price: parseFloat(price), availableSeats, instructor, image: imgURL }
+                    axiosSecure.post("/allClass", newItem)
+                        .then(data => {
+                            if (data.data.insertedId) {
+                                reset();
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'Class added successfully',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            }
+                        })
+                }
+            })
 
     };
 
 
     return (
         <div className="w-full px-10">
-            {/* <SectionTitle subHeading="What's new" heading="Add an item" ></SectionTitle> */}
+            <Helmet>Music School - Add class</Helmet>
+            <Title heading={'add a class'}></Title>
             <form onSubmit={handleSubmit(onSubmit)} className='p-8 bg-violet-200'>
                 <div className="form-control w-full mb-4">
                     <label className="label">
@@ -99,7 +100,7 @@ const AddClass = () => {
                     </label>
                     <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full " />
                 </div>
-                <input className="btn btn-sm mt-4 bg-blue-300 hover:bg-blue-500" type="submit" value="Add Class"/>
+                <input className="btn btn-sm mt-4 bg-blue-300 hover:bg-blue-500" type="submit" value="Add Class" />
             </form>
         </div>
     );
